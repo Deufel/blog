2127d9e9c77cedc8bf569c675f0d79e50252fc85
Deufel <MDeufel13@gmail.com>
2025-07-19 00:15:35 -0500

    save

--- view_transition.js
+++ view_transition.js
@@ -10,13 +10,27 @@ document.addEventListener("htmx:beforeSwap", function (evt) {
   evt.detail.serverResponse = doc.documentElement.outerHTML;
 });
 
+// document.addEventListener("htmx:afterSwap", function (evt) {
+//   // Clean up view-transition-name after the transition completes
+//   setTimeout(() => {
+//     evt.target
+//       .querySelectorAll('[style*="view-transition-name"]')
+//       .forEach((element) => {
+//         element.style.viewTransitionName = "";
+//       });
+//   }, 500); // Wait for transition to complete
+// });
+
+// Clean up view-transition-name after the transition completes
 document.addEventListener("htmx:afterSwap", function (evt) {
-  // Clean up view-transition-name after the transition completes
-  setTimeout(() => {
-    evt.target
-      .querySelectorAll('[style*="view-transition-name"]')
-      .forEach((element) => {
-        element.style.viewTransitionName = "";
-      });
-  }, 500); // Wait for transition to complete
+  document.addEventListener("viewtransition", function handler(e) {
+    if (e.viewTransition.finished) {
+      evt.target
+        .querySelectorAll('[style*="view-transition-name"]')
+        .forEach((element) => {
+          element.style.viewTransitionName = "";
+        });
+      document.removeEventListener("viewtransition", handler);
+    }
+  });
 });
