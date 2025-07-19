294a818d843274f79f5e35639a5292dcc36e8955
Deufel <MDeufel13@gmail.com>
2025-07-18 20:20:38 -0500

    save

--- view_transition.js
+++ view_transition.js
@@ -1,10 +1,13 @@
+window.vtCount ??= 1;
+
 document.addEventListener("htmx:beforeSwap", function (evt) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(evt.detail.xhr.responseText, "text/html");
   const slideElements = doc.querySelectorAll("[vt-slide-in-right]");
 
   slideElements.forEach((element) => {
-    element.style.viewTransitionName = "slide-right";
+    const uniqueId = "vt__" + window.vtCount++;
+    element.style.viewTransitionName = uniqueId;
   });
 
   evt.detail.serverResponse = doc.documentElement.outerHTML;
