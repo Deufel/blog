8ecb328460eff2ae0bb749f9fa97b7091b28dc6c
Deufel <MDeufel13@gmail.com>
2025-07-18 19:58:21 -0500

    save

--- view_transition.js
+++ view_transition.js
@@ -1,9 +1,12 @@
 document.addEventListener("htmx:load", function (evt) {
   console.log("HTMX load event fired!", evt.detail.elt);
+
   const slideElements = evt.detail.elt.querySelectorAll("[vt-slide-in-right]");
+  console.log("Found slide elements:", slideElements.length, slideElements);
 
   slideElements.forEach((element) => {
-    // Set a specific view-transition-name
+    console.log("Setting view-transition-name on:", element);
     element.style.viewTransitionName = "slide-right";
+    console.log("Style after setting:", element.style.viewTransitionName);
   });
 });
